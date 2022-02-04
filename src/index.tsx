bd1c4e4703f28b31a64eb85100d29f0513d3bccc
محمد دربندی <darbandi.craft@gmail.com>
2022-02-04 14:56:43 +0330

    changed remixicon address

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "remixicon/fonts/remixicon.css";
 import "./assets/sass/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
